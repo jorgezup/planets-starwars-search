98542647f5dd2565b1a81efd3065e501dd375013
Jorge Zupirolli <jorgebertolo@gmail.com>
2022-03-22 19:14:13 -0400

    refactor: added react lazy and suspense

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,9 @@
-import React, { useEffect, useContext } from 'react';
-import Table from '../components/Table';
+import React, { useEffect, useContext, Suspense } from 'react';
+import Loading from '../components/Loading';
 import Context from '../context/context';
 import getPlanets from '../services/api';
+
+const Table = React.lazy(() => import('../components/Table'));
 
 const Home = () => {
   const { setPlanets } = useContext(Context);
@@ -14,7 +16,9 @@ const Home = () => {
     fetchData();
   }, [setPlanets]);
   return (
-    <Table />
+    <Suspense fallback={ <Loading /> }>
+      <Table />
+    </Suspense>
   );
 };
 
