a8150c05938939eb6e78e5f1930c5f21360aee8a
Jorge Zupirolli <jorgebertolo@gmail.com>
2022-03-22 23:31:03 -0400

    chore: constant file

---
+++ src/utils/constants.js
@@ -0,0 +1,13 @@
+export const COLUMN_FILTER = [
+  'population',
+  'orbital_period',
+  'diameter',
+  'rotation_period',
+  'surface_water',
+];
+
+export const COMPARISON_FILTER = [
+  'maior que',
+  'menor que',
+  'igual a',
+];
