bd5806d7d41933dfea7fa1734557edb20d4c0738
Jorge Zupirolli <jorgebertolo@gmail.com>
2022-03-22 22:43:02 -0400

    feat: added input and handleChange and setFilter

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,14 @@
-import React, { useEffect, useContext, Suspense } from 'react';
+import React, { useEffect, useContext, Suspense, useState } from 'react';
 import Loading from '../components/Loading';
 import Context from '../context/context';
 import getPlanets from '../services/api';
 
 const Table = React.lazy(() => import('../components/Table'));
+const Input = React.lazy(() => import('../components/Input'));
 
 const Home = () => {
-  const { setPlanets } = useContext(Context);
+  const { setPlanets, setFilters } = useContext(Context);
+  const [filterByName, setFilterByName] = useState('');
 
   useEffect(() => {
     async function fetchData() {
@@ -15,8 +17,25 @@ const Home = () => {
     }
     fetchData();
   }, [setPlanets]);
+
+  const handleChange = ({ target: { value } }) => {
+    setFilterByName(value);
+  };
+
+  useEffect(() => {
+    setFilters((prevState) => ({ ...prevState, filterByName }));
+  }, [filterByName, setFilters]);
+
   return (
     <Suspense fallback={ <Loading /> }>
+      <Input
+        label="filtrar"
+        name="Filtrar"
+        type="text"
+        data-testid="name-filter"
+        value={ filterByName }
+        onChange={ handleChange }
+      />
       <Table />
     </Suspense>
   );
