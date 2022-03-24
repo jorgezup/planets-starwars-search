a14ba574689921c1d1cf10e4e1504583575b7a4c
Jorge Zupirolli <jorgebertolo@gmail.com>
2022-03-24 17:38:38 -0400

    feat: added function sortPlanets

--- src/context/provider.js
+++ src/context/provider.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 
 import Context from './context';
+import { MINUS_ONE } from '../utils/constants';
 
 const Provider = ({ children }) => {
   const [planets, setPlanets] = useState([]);
@@ -9,12 +10,35 @@ const Provider = ({ children }) => {
     filterByName: '',
     filterBy: [],
   });
+  const [isSortedClicked, setIsSortedClicked] = useState(false);
+  const [sortState, setSortState] = useState({
+    order: {
+      column: 'population',
+      sort: 'ASC',
+    },
+  });
+
+  const sortPlanets = (array) => {
+    const { order } = sortState;
+    return array.sort((a, b) => {
+      if (a[order.column] === 'unknown') return 1;
+      if (b[order.column] === 'unknown') return MINUS_ONE;
+      if (order.sort === 'ASC') return a[order.column] - b[order.column];
+      if (order.sort === 'DESC') return b[order.column] - a[order.column];
+      return true;
+    });
+  };
 
   const context = {
     planets,
     setPlanets,
     filters,
     setFilters,
+    isSortedClicked,
+    setIsSortedClicked,
+    sortState,
+    setSortState,
+    sortPlanets,
   };
 
   return (
